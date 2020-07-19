5026ca9481cf30fbdcf4bf3d5eb5e607bc8b784e
sourabhsdx <40622967+sourabhsdx@users.noreply.github.com>
2020-07-19 16:51:57 +0530

    design updated

--- lib/app/home/home.dart
+++ lib/app/home/home.dart
@@ -23,203 +23,252 @@ class _HomePageState extends State<HomePage> {
         leading: Icon(Icons.bluetooth),
         title: Text("Bluetooth Control"),
       ),
-      body: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10)),
-          color: Colors.white
-        ),
-        child: Column(
-          children: <Widget>[
-            FutureBuilder<List<BluetoothDevice>>(
-              future: bluetooth.getDevices(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasData) {
+      body: RefreshIndicator(
+        // ignore: missing_return
+        onRefresh: () async {
+          await sharedPref.getTurnOffFan();
+          setState(() {});
+        },
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(topLeft: Radius.circular(10),topRight: Radius.circular(10)),
+            color: Colors.white
+          ),
+          child: ListView(
+            children: <Widget>[
+              FutureBuilder<List<BluetoothDevice>>(
+                future: bluetooth.getDevices(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasData) {
+                      return Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Column(
+                          children: <Widget>[
+                            DropdownButtonFormField(
+                              items: snapshot.data
+                                  .map((e) => DropdownMenuItem(
+                                        child: Text(e.name),
+                                        value: e.address,
+                                      ))
+                                  .toList(),
+                              onChanged: (value) {
+                                setState(() {
+                                  _value = value;
+                                  print(_value);
+                                });
+                              },
+                              value: _value,
+                            ),
+                            RaisedButton(
+                              child: Text(_connected ? "Disconnect" : "Connect"),
+                              onPressed: _connected
+                                  ? () => _disconnect(context)
+                                  : () => _connect(context),
+                            )
+                          ],
+                        ),
+                      );
+                    }
                     return Padding(
-                      padding: const EdgeInsets.all(16.0),
-                      child: Column(
-                        children: <Widget>[
-                          DropdownButtonFormField(
-                            items: snapshot.data
-                                .map((e) => DropdownMenuItem(
-                                      child: Text(e.name),
-                                      value: e.address,
-                                    ))
-                                .toList(),
-                            onChanged: (value) {
-                              setState(() {
-                                _value = value;
-                                print(_value);
-                              });
-                            },
-                            value: _value,
-                          ),
-                          RaisedButton(
-                            child: Text(_connected ? "Disconnect" : "Connect"),
-                            onPressed: _connected
-                                ? () => _disconnect(context)
-                                : () => _connect(context),
-                          )
-                        ],
-                      ),
+                      padding: EdgeInsets.all(10.0),
+                      child: Text("Turn on bluetooth"),
                     );
                   }
-                  return Padding(
-                    padding: EdgeInsets.all(10.0),
-                    child: Text("Turn on bluetooth"),
-                  );
-                }
-                return CircularProgressIndicator();
-              },
-            ),
-            Text("Status:"),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListTile(
-                leading: Icon(Icons.lightbulb_outline),
-                title: FutureBuilder<bool>(
-                  future: sharedPref.getStatus('light'),
-                  builder: (context, snapshot) {
-                    if (snapshot.connectionState == ConnectionState.done) {
-                      if (snapshot.hasData) {
-                        return snapshot.data ? Text('Status : On ') : Text('Status : Off ');
-                      }
-                      return Text('Off ');
-                    }
-                    return Text('unknown');
-                  },
-                ),
-                subtitle: FutureBuilder<String>(
-                  future: sharedPref.getTurnOnLight(),
-                  builder: (context,snapshot){
-                    if(snapshot.connectionState==ConnectionState.done){
-                      if(snapshot.hasData){
-                        return Text("Last turned on: ${snapshot.data}");
-                      }
-                      return Text('unknown');
-                    }
-                    return Text('unknown');
-                  },
-                ),
-                trailing: FutureBuilder<String>(
-                  future: sharedPref.getTurnOffLight(),
-                  builder: (context,snapshot){
-                    if(snapshot.connectionState==ConnectionState.done){
-                      if(snapshot.hasData){
-                        return Text("Last turned off: ${snapshot.data}");
-                      }
-                      return Text('unknown');
-                    }
-                    return Text('unknown');
-                  },
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListTile(
-                leading: Icon(Icons.ac_unit),
-              title: FutureBuilder<bool>(
-                future: sharedPref.getStatus('fan'),
-                builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done) {
-                if (snapshot.hasData) {
-                  return snapshot.data ? Text('Status : On ') : Text('Status : Off ');
-                }
-                return Text('Off ');
-              }
-              return Text('unknown ');
+                  return CircularProgressIndicator();
                 },
               ),
-                subtitle: FutureBuilder<String>(
-                  future: sharedPref.getTurnOnFan(),
-                  builder: (context,snapshot){
-                    if(snapshot.connectionState==ConnectionState.done){
-                      if(snapshot.hasData){
-                        return Text("Last turned on: ${snapshot.data}");
+
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(Icons.lightbulb_outline),
+                  title: FutureBuilder<bool>(
+                    future: sharedPref.getStatus('light'),
+                    builder: (context, snapshot) {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        if (snapshot.hasData) {
+                          return snapshot.data ? Text('Status : On ') : Text('Status : Off ');
+                        }
+                        return Text('Off ');
                       }
                       return Text('unknown');
-                    }
-                    return Text('unknown');
-                  },
-                ),
-                trailing:   FutureBuilder<String>(
-                  future: sharedPref.getTurnOffFan(),
-                  builder: (context,snapshot){
-                    if(snapshot.connectionState==ConnectionState.done){
-                      if(snapshot.hasData){
-                        return Text("Last turned off: ${snapshot.data}");
+                    },
+                  ),
+                  subtitle: FutureBuilder<String>(
+                    future: sharedPref.getTurnOnLight(),
+                    builder: (context,snapshot){
+                      if(snapshot.connectionState==ConnectionState.done){
+                        if(snapshot.hasData){
+                          return Text("Last turned on: ${snapshot.data}");
+                        }
+                        return Text('unknown');
                       }
                       return Text('unknown');
-                    }
-                    return Text('unknown');
-                  },
+                    },
+                  ),
+                  trailing: FutureBuilder<String>(
+                    future: sharedPref.getTurnOffLight(),
+                    builder: (context,snapshot){
+                      if(snapshot.connectionState==ConnectionState.done){
+                        if(snapshot.hasData){
+                          return Text("Last turned off: ${snapshot.data}");
+                        }
+                        return Text('unknown');
+                      }
+                      return Text('unknown');
+                    },
+                  ),
                 ),
               ),
-            ),
-            Material(
-              elevation: 0,
-              child: ButtonBar(
-                alignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text("Light"),
-                  FlatButton(
-                    child: Text("On"),
-                    onPressed: !_connected
-                        ? null
-                        : () {
-                            bluetooth.write("L");
-                            sharedPref.setStatus('light', true);
-                            sharedPref.setTurnOnLight();
-                            setState(() {});
-                          },
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListTile(
+                  leading: Icon(Icons.ac_unit),
+                title: FutureBuilder<bool>(
+                  future: sharedPref.getStatus('fan'),
+                  builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done) {
+                  if (snapshot.hasData) {
+                    return snapshot.data ? Text('Status : On ') : Text('Status : Off ');
+                  }
+                  return Text('Off ');
+                }
+                return Text('unknown ');
+                  },
+                ),
+                  subtitle: FutureBuilder<String>(
+                    future: sharedPref.getTurnOnFan(),
+                    builder: (context,snapshot){
+                      if(snapshot.connectionState==ConnectionState.done){
+                        if(snapshot.hasData){
+                          return Text("Last turned on: ${snapshot.data}");
+                        }
+                        return Text('unknown');
+                      }
+                      return Text('unknown');
+                    },
                   ),
-                  FlatButton(
-                    child: Text("OFF"),
-                    onPressed: !_connected
-                        ? null
-                        : () {
-                            bluetooth.write("7");
-                            sharedPref.setStatus('light', false);
-                            sharedPref.setTurnOffLight();
-                            setState(() {});
-                          },
+                  trailing:   FutureBuilder<String>(
+                    future: sharedPref.getTurnOffFan(),
+                    builder: (context,snapshot){
+                      if(snapshot.connectionState==ConnectionState.done){
+                        if(snapshot.hasData){
+                          return Text("Last turned off: ${snapshot.data}");
+                        }
+                        return Text('unknown');
+                      }
+                      return Text('unknown');
+                    },
                   ),
-                ],
+                ),
               ),
-            ),
-            Material(
-              elevation: 1,
-              child: ButtonBar(
-                alignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Text("Fan"),
-                  FlatButton(
-                    child: Text("On"),
-                    onPressed: !_connected
-                        ? null
-                        : () {
-                            bluetooth.write("F");
-                            sharedPref.setStatus('fan', true);
-                            sharedPref.setTurnOnFan();
-                            setState(() {});
-                          },
-                  ),
-                  FlatButton(
-                    child: Text("OFF"),
-                    onPressed: !_connected
-                        ? null
-                        : () {
-                            bluetooth.write("3");
-                            sharedPref.setStatus('fan', false);
-                            sharedPref.setTurnOffFan();
-                            setState(() {});
-                          },
-                  ),
-                ],
+              Material(
+                elevation: 0,
+                child: ButtonBar(
+                  alignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text("Light"),
+                    FutureBuilder<bool>(
+                      future: sharedPref.getStatus('light'),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          if (snapshot.hasData) {
+                            return snapshot.data ?  FlatButton(
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.flash_off),
+                                  Text("OFF"),
+                                ],
+                              ),
+                              onPressed: !_connected
+                                  ? null
+                                  : () {
+                                bluetooth.write("7");
+                                sharedPref.setStatus('light', false);
+                                sharedPref.setTurnOffLight();
+                                setState(() {});
+                              },
+                            ) : FlatButton(
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.flash_on),
+                                  Text("On"),
+                                ],
+                              ),
+                              onPressed: !_connected
+                                  ? null
+                                  : () {
+                                bluetooth.write("L");
+                                sharedPref.setStatus('light', true);
+                                sharedPref.setTurnOnLight();
+                                setState(() {});
+                              },
+                            );
+                          }
+                          return Text('Off ');
+                        }
+                        return Text('unknown');
+                      },
+                    )
+                    ,
+                  ],
+                ),
               ),
-            )
-          ],
+              Material(
+                elevation: 1,
+                child: ButtonBar(
+                  alignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text("Fan"),
+                    FutureBuilder<bool>(
+                      future: sharedPref.getStatus('fan'),
+                      builder: (context, snapshot) {
+                        if (snapshot.connectionState == ConnectionState.done) {
+                          if (snapshot.hasData) {
+                            return snapshot.data ? FlatButton(
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.flash_off),
+                                  Text("OFF"),
+                                ],
+                              ),
+                              onPressed: !_connected
+                                  ? null
+                                  : () {
+                                bluetooth.write("3");
+                                sharedPref.setStatus('fan', false);
+                                sharedPref.setTurnOffFan();
+                                setState(() {});
+                              },
+                            ) :  FlatButton(
+                              child: Row(
+                                children: <Widget>[
+                                  Icon(Icons.flash_on),
+                                  Text("On"),
+                                ],
+                              ),
+                              onPressed: !_connected
+                                  ? null
+                                  : () {
+                                bluetooth.write("F");
+                                sharedPref.setStatus('fan', true);
+                                sharedPref.setTurnOnFan();
+                                setState(() {});
+                              },
+                            );
+                          }
+                          return Text('Off ');
+                        }
+                        return Text('unknown ');
+                      },
+                    )
+                    ,
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
